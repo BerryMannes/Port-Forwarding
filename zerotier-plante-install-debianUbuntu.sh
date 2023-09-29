dc09b7f6788aaefad2f164a3b60895c370ac63cb
BerryMannes <146300679+BerryMannes@users.noreply.github.com>
2023-09-29 10:24:16 +0800

    Update zerotier-plante-install-debianUbuntu.sh

--- zerotier-plante-install-debianUbuntu.sh
+++ zerotier-plante-install-debianUbuntu.sh
@@ -3,7 +3,7 @@
 # 下面一条代码取消注释后为自动获取Addr服务器公网ip
 # ip=`wget http://ipecho.net/plain -O - -q ; echo` 
 # 下面一条代码取消注释后为手动设置Addr服务器公网ip
- ip=`39.103.212.40` 
+ip=`39.103.212.40` 
 # 设置https访问端口
 httpsprot=9877
 # 设置服务端口
@@ -39,7 +39,7 @@ cd /opt/key-networks/ztncui/
 echo "HTTPS_PORT = $httpsprot" >>./.env 
 secret=`cat /var/lib/zerotier-one/authtoken.secret` 
 echo "ZT_TOKEN = $secret" >>./.env 
-echo "ZT_ADDR=39.103.212.40:$serverport" >>./.env 
+echo "ZT_ADDR=$ip:$serverport" >>./.env 
 echo "NODE_ENV = production" >>./.env 
 echo "HTTP_ALL_INTERFACES=yes" >>./.env 
 systemctl restart ztncui 
